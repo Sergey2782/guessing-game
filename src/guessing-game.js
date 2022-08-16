49dc06902778f78172e43f7725e5daeffc06e169
Sergei Malkov <msergun234@gmail.com>
2022-08-17 02:42:08 +0300

    g game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.min_value = 0;
+      this.max_value = 0;
+      this.res = 0;
+    }
+  
     setRange(min, max) {
-
+      this.min_value = min+1;
+      this.max_value = max;
     }
-
+  
     guess() {
-
+      this.res = Math.floor(
+        this.min_value + (this.max_value - this.min_value) / 2
+      );
+      return this.res;
     }
-
+  
     lower() {
-
+      this.max_value = this.res;
     }
-
+  
     greater() {
-
+      this.min_value = this.res+1;
     }
-}
-
-module.exports = GuessingGame;
+  }
+  
+  
+  module.exports = GuessingGame;
